b49699970838e0cdd73203fc8702170f6918be67
shilpa.sarath <shilpa.sarath@jenesystech.com>
2023-03-20 16:05:01 +0530

    This is our initial state.

--- Sample/TestScript.py
+++ Sample/TestScript.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.keys import Keys
 driver: WebDriver = webdriver.Chrome('./chromedriver')
 driver.get("https://www.python.org")
 print(driver.title)
-search_bar = driver.find_element_by_name("q")
+search_bar = driver.find_element("name", "q")
+search_bar
 search_bar.clear()
 search_bar.send_keys("getting started with python")
 search_bar.send_keys(Keys.RETURN)
